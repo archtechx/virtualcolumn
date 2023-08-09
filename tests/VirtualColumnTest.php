4414d112038512a4dc76ac0b5f992b8336ef55c6
lukinovec <lukinovec@gmail.com>
2023-08-09 12:39:52 +0200

    Delete get() and set() types

--- tests/VirtualColumnTest.php
+++ tests/VirtualColumnTest.php
@@ -189,22 +189,12 @@ class FooModel extends Model
 
 class EncryptedCast implements CastsAttributes
 {
-    /**
-     * Cast the given value.
-     *
-     * @param  array<string, mixed>  $attributes
-     */
-    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    public function get($model, $key, $value, $attributes)
     {
         return Crypt::decryptString($value);
     }
 
-    /**
-     * Prepare the given value for storage.
-     *
-     * @param  array<string, mixed>  $attributes
-     */
-    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    public function set($model, $key, $value, $attributes)
     {
         return Crypt::encryptString($value);
     }
